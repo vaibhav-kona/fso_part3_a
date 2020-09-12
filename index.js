fc02529072f30be1a24e0de96637b7c4408836e6
Vaibhav <vaibhav@bluesapling.com>
2020-09-12 12:09:12 +0530

    Complete exercise 3.4

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const app = express();
 
-let persons = [
-  {
+let persons = [{
     id: 1,
     name: 'Harshita',
     number: 'abcd'
@@ -24,17 +23,41 @@ app.get('/api/persons', (req, res) => {
 });
 
 app.get('/api/persons/:personId', (req, res) => {
-  const { personId } = req.params;
+  const {
+    personId
+  } = req.params;
 
   if (personId) {
     const matchingPersonId = persons.findIndex(
       (person) => person.id.toString() === personId.toString()
     );
-    res.json({ data: persons[matchingPersonId] });
+    res.json({
+      data: persons[matchingPersonId]
+    });
   } else {
     res.status(404).end();
   }
 });
+
+app.delete('/api/persons/:personId', (req, res) => {
+  const {
+    personId
+  } = req.params;
+  if (personId) {
+    const matchingPersonIndex = persons.findIndex(
+      (person) => person.id.toString() === personId.toString()
+    );
+
+    if (matchingPersonIndex >= 0) {
+      persons.splice(matchingPersonIndex, 1);
+      res.status(200).end();
+    }
+
+    res.status(404).end();
+  }
+
+  res.status(404).end();
+})
 
 app.get('/info', (req, res) => {
   const totalPersonsInfo = `<p>Phonebook has info for ${persons.length} people</p>`;

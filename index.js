fa40be951e4ff912c669c0c0ac2290a1599c4356
Vaibhav <vaibhav@bluesapling.com>
2020-09-19 17:26:25 +0530

    Add procfile
    - Using port provided by env now instead of directly specifying the
      port.

--- index.js
+++ index.js
@@ -129,7 +129,7 @@ app.get('/info', (req, res) => {
   res.send(`${totalPersonsInfo} ${date} `)
 });
 
-const PORT = 3002;
+const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT} `);
 });

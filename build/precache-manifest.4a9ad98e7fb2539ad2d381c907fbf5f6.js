29b63aef9a7abc9d6c43352432fac274521a0f2a
Vaibhav <vaibhav@bluesapling.com>
2020-09-19 18:52:54 +0530

    uibuild

---
+++ build/precache-manifest.4a9ad98e7fb2539ad2d381c907fbf5f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b867eba7cc1df9a0f1fd12b78ad9a092",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b3e21a67998afdab590",
+    "url": "/static/css/main.087787dd.chunk.css"
+  },
+  {
+    "revision": "58bd7345623f9bd76703",
+    "url": "/static/js/2.e3d3a486.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e3d3a486.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b3e21a67998afdab590",
+    "url": "/static/js/main.3af2fe6b.chunk.js"
+  },
+  {
+    "revision": "37e42bc47805ba865915",
+    "url": "/static/js/runtime-main.fac6ad05.js"
+  }
+]);

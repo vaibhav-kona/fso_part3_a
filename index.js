aba36b234a4902cb6556cda0917f18fbf86bc169
Vaibhav <vaibhav@bluesapling.com>
2020-09-22 18:03:06 +0530

    Finish 3.15 to 3.18

--- index.js
+++ index.js
@@ -13,8 +13,9 @@ morgan.token('post-data-object', (req, res) => {
 })
 
 app.use(cors());
-app.use(express.json());
+
 app.use(express.static('build'));
+app.use(express.json());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-data-object'));
 
 const Person = require('./models/person');
@@ -28,34 +29,29 @@ app.get('/api/persons', (req, res) => {
 app.get('/api/persons/:personId', (req, res) => {
   const { personId } = req.params;
 
-  if (personId) {
-    Person.findById(personId)
-      .then((person) => {
-        res.json({ data: person });
-      })
-  } else {
-    res.status(404).end();
-  }
+  Person.findById(req.params.personId)
+    .then((person) => {
+      if (person) {
+        res.json(person);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((err) => next(err));
 });
 
 app.delete('/api/persons/:personId', (req, res) => {
   const {
     personId
   } = req.params;
-  // if (personId) {
-  //   const matchingPersonIndex = persons.findIndex(
-  //     (person) => person.id.toString() === personId.toString()
-  //   );
 
-  //   if (matchingPersonIndex >= 0) {
-  //     persons.splice(matchingPersonIndex, 1);
-  //     res.status(200).end();
-  //   }
+  Person.findByIdAndDelete(req.params.personId)
+    .then(() => {
+      res.status(204).end();
+    })
+    .catch((err) => next(err));
 
-  //   res.status(404).end();
-  // }
 
-  res.status(404).end();
 })
 
 app.post('/api/persons/', (req, res) => {
@@ -112,6 +108,17 @@ app.post('/api/persons/', (req, res) => {
   }
 })
 
+app.put('/api/persons/:personId', (req, res) => {
+  const { name, number } = req.body;
+  const person = { name, number };
+
+  Person.findByIdAndUpdate(req.params.personId, person, { new: true })
+    .then((updatedPerson) => {
+      res.status(201).send(updatedPerson);
+    })
+    .catch((err) => next(err));
+})
+
 app.get('/info', (req, res) => {
   Person.find({}).then((persons) => {
     const totalPersonsInfo = `<p>Phonebook has info for ${persons.length} people</p>`;
@@ -120,6 +127,24 @@ app.get('/info', (req, res) => {
     res.send(`${totalPersonsInfo} ${date} `);
   })
 });
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT;
 const HOST = '0.0.0.0';
